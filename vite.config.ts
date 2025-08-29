b391f597a6d72f07882f890ac6f02a3cfd818e1c
t0mg <t0mg@users.noreply.github.com>
2025-08-29 23:11:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-      base: '/',
+      base: '/rogue-steward/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
