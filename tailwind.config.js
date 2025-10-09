7cb22680300b5fe8a669b717e6613948b7788aa3
t0mg <tom@t0m.fr>
2025-10-09 10:51:43 +0200

    ui tweaks; background image (unoptimized)

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,25 +19,28 @@ export default {
         'rarity-uncommon': '#9e8100ff',
         'rarity-rare': '#8d1dceff',
       },
+      fontFamily: {
+        sans: 'inherit',
+      },
       keyframes: {
         'fade-in': {
-            '0%': { opacity: '0', transform: 'scale(0.95)' },
-            '100%': { opacity: '1', transform: 'scale(1)' },
+          '0%': { opacity: '0', transform: 'scale(0.95)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
         },
         'fade-in-up': {
-            '0%': { opacity: '0', transform: 'translateY(20px)' },
-            '100%': { opacity: '1', transform: 'translateY(0)' },
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         },
         'newly-drafted-animation': {
-            '0%': { boxShadow: '0 0 0 0 rgba(255, 215, 0, 0)', transform: 'scale(1.0)' },
-            '50%': { boxShadow: '0 0 10px 5px rgba(255, 215, 0, 0.5)', transform: 'scale(1.05)' },
-            '100%': { boxShadow: '0 0 0 0 rgba(255, 215, 0, 0)', transform: 'scale(1.0)' },
+          '0%': { boxShadow: '0 0 0 0 rgba(255, 215, 0, 0)', transform: 'scale(1.0)' },
+          '50%': { boxShadow: '0 0 10px 5px rgba(255, 215, 0, 0.5)', transform: 'scale(1.05)' },
+          '100%': { boxShadow: '0 0 0 0 rgba(255, 215, 0, 0)', transform: 'scale(1.0)' },
         },
       },
       animation: {
-          'fade-in': 'fade-in 0.3s ease-out forwards',
-          'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
-          'newly-drafted': 'newly-drafted-animation 1s ease-in-out',
+        'fade-in': 'fade-in 0.3s ease-out forwards',
+        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'newly-drafted': 'newly-drafted-animation 1s ease-in-out',
       }
     },
   },
