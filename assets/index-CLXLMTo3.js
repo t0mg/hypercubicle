e3ef945a3266387e2bf41befbeb90f15951f9c76
t0mg <t0mg@users.noreply.github.com>
2025-10-12 22:00:09 +0000

    deploy: 21af0048e1216f5df0e364119af52c24df3ae985

--- assets/index-CLXLMTo3.js
+++ assets/index-CLXLMTo3.js
@@ -281,7 +281,7 @@
         </div>
         <div class="status-bar">
           <p class="status-bar-field">v0.0.0</p>
-          <p class="status-bar-field">build 123</p>
+          <p class="status-bar-field">build 125</p>
         </div>
       </div>
     `}}customElements.define("menu-screen",Ze);class Je extends HTMLElement{constructor(){super(),this.isDesktop=!0,this.attachShadow({mode:"open"}),this.onclick=t=>{window.matchMedia("(pointer: coarse)").matches&&t.target===this&&this.hide()};const e=document.createElement("style");e.textContent=`
