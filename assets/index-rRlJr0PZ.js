a6df0627933bd5b693d44d4eec1c7c35d9b37bab
t0mg <t0mg@users.noreply.github.com>
2025-10-14 11:13:25 +0000

    deploy: 9e5ae612523b207a9d9e77340b18262d96bee87d

--- assets/index-rRlJr0PZ.js
+++ assets/index-rRlJr0PZ.js
@@ -281,7 +281,7 @@
         </div>
         <div class="status-bar">
           <p class="status-bar-field">v0.0.0</p>
-          <p class="status-bar-field">build 138</p>
+          <p class="status-bar-field">build 139</p>
         </div>
       </div>
     `}}customElements.define("menu-screen",it);class at extends HTMLElement{constructor(){super(),this.isDesktop=!0,this.attachShadow({mode:"open"}),this.onclick=t=>{window.matchMedia("(pointer: coarse)").matches&&t.target===this&&this.hide()};const e=document.createElement("style");e.textContent=`
